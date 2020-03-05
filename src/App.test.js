214e4b2f8b189eb8655ef602e826979623488751
Nitin Sood <nitin.sood01@nagarro.com>
2020-03-06 01:46:55 +0530

    added test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { render, mount, findByLabelText } from "@testing-library/react";
+import App from "./App";
+import reducers from "./reducers";
+import SearchCampaign from "./containers/SearchCampaign";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders few records", () => {
+  const store = createStore(reducers, applyMiddleware(thunk));
+  const wrapper = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+
+  expect(document.querySelector(".campaign-data-tbl")).toBeInstanceOf(
+    HTMLElement
+  );
 });
